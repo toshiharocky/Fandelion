edfb0e44c4c011e53fc026a19a1e4cc4e15dfd75
EC2 Default User <ec2-user@ip-172-31-3-253.ec2.internal>
2021-07-18 07:15:57 +0000

    微修正

--- database/seeds/GuestGenderTableSeeder.php
+++ database/seeds/GuestGenderTableSeeder.php
@@ -9,28 +9,31 @@ class GuestGenderTableSeeder extends Seeder
      *
      * @return void
      */
+     
+     
+     
     public function run()
     {
         \DB::table('guest_genders')->insert([
         [
-            'id' => 1;
-            'guest_gender' => '特になし';
+            'id' => 1,
+            'guest_gender' => '特になし',
          ],
         [
-            'id' => 2;
-            'guest_gender' => '女性限定';
+            'id' => 2,
+            'guest_gender' => '女性限定',
          ],
         [
-            'id' => 3;
-            'guest_gender' => '男性限定';
+            'id' => 3,
+            'guest_gender' => '男性限定',
          ],
         [
-            'id' => 4;
-            'guest_gender' => '女性限定(女性同伴の場合は男性も可)';
+            'id' => 4,
+            'guest_gender' => '女性限定(女性同伴の場合は男性も可)',
          ],
         [
-            'id' => 5;
-            'guest_gender' => '男性限定(男性同伴の場合は女性も可)';
+            'id' => 5,
+            'guest_gender' => '男性限定(男性同伴の場合は女性も可)',
          ],
          
     ]);
