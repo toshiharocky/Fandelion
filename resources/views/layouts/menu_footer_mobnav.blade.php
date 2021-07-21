d1b3ff030f741e11cd539ca65298459ccd2eb7b8
EC2 Default User <ec2-user@ip-172-31-3-253.ec2.internal>
2021-07-21 01:44:08 +0000

    スケールの設定を変更

--- resources/views/layouts/menu_footer_mobnav.blade.php
+++ resources/views/layouts/menu_footer_mobnav.blade.php
@@ -5,7 +5,8 @@
 ================================================== -->
 <title>Fandelion</title>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">-->
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 
 <!-- CSS
 ================================================== -->
--- resources/views/layouts/menu_mobnav.blade.php
+++ resources/views/layouts/menu_mobnav.blade.php
@@ -5,8 +5,8 @@
 ================================================== -->
 <title>Fandelion</title>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
+<!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">-->
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 
 
 <link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css">
