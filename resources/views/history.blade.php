4786aad3f350366aae5b1c47da9149b111136ef9
EC2 Default User <ec2-user@ip-172-31-3-253.ec2.internal>
2021-07-21 06:15:26 +0000

    試験的

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -57,6 +57,7 @@ let booking_check = "{{$booking_check}}";
 // console.log(booking_check == 0);
 // 現在の日付を取得する
 let now = new Date().toISOString();
+<!--console.log(now);-->
 
 
 
@@ -222,121 +223,7 @@ function future(){
                 		<!--// nowがキャンセル期間よりも前の場合、#cancelに「予約の修正」「予約のキャンセル」ボタンを表示し、-->
                 		<!--//「予約の修正・キャンセルはyyyy/mm/dd H:iまでです」と表示-->
                 		<!--// nowがキャンセル期間を超過した場合、#cancelに「予約の修正・キャンセル期間は終了しました」と表示-->let cancel_id = "cancel_"+$i;
-                         if(bookingstatus_id[$i]=='20'){
-                		    $(check_in_out_id).append(
-                    			`
-                    			<form id="check_out" method="put" action="check_out">
-                    		        <input type="hidden" name="gym_id" value=${gym_id[$i]}>
-                    		        <input type="hidden" name="booking_id" value=${booking_id[$i]}>
-                    		        <h5>現在トレーニング中です<h5>
-                    			    <input type="submit" class="button gray" style="width:100%; height:35px;" value="トレーニング終了">
-                			    </form>
-                    			`
-                		    );
-                        }else{
-                		
-                    		if(now < cancel_limit){
-                    		    $(cancel_id).append(
-                    		        `
-                    		        <form method="post" action="booking_update">
-                    		        @csrf
-                        		        <input type="hidden" name="gym_id" value=${gym_id[$i]}>
-                        		        <input type="hidden" name="booking_id" value=${booking_id[$i]}>
-                        		        <input type="submit" class="button gray" style="width:100%; height:35px;" value="予約の修正">
-                    		        </from>
-                    		        <form method="post" action="booking_cancel">
-                    		        @csrf
-                        		        <input type="hidden" name="gym_id" value=${gym_id[$i]}>
-                        		        <input type="hidden" name="booking_id" value=${booking_id[$i]}>
-                            			<input type="submit" class="button gray" style="width:100%; height:35px;" value="予約のキャンセル">
-                        			</from>
-                        			<h5>予約の修正・キャンセルは<br>${cancel_date} ${cancel_time}までです。<h5>
-                        			`
-                    		    )
-                    		    
-                    		}else{
-                    		    if(now < checkin_open){
-                        		    $(cancel_id).append(
-                        		        `
-                        		        <h5>チェックインは<br>開始時間15分前から可能です。<h5>
-                            			<form id="check_in" method="put" action="check_in">
-                            			    <input type="submit" class="button gray" style="width:100%; height:35px; background-color:#dcdcdc; color:white;" value="トレーニング開始" disabled>
-                        			    </form>
-                            			<h5>予約の修正・キャンセル期間は<br>終了しました。<h5>
-                            			`
-                        		    )
-                    		    }
-                    		}
-                    		
-                          
-                    		
-                    		　
-                    		<!--予約の修正ボタンを押すと、修正画面へ移る-->
-                    		<!--予約のキャンセルを押すと、「予約をキャンセルしますか？（この処理は取り消すことができません）」のconfirmが出てキャンセルする-->
-                    		
-                    		
-                    		
-                    		<!--// nowが開始15分前の場合、history_buttonsに「トレーニング開始」ボタンを表示-->
-                    		if(now > checkin_open){
-                        		<!--// dateが開始時間を超過し、bookingstatus_idが「1」「5」の場合、history_buttonsに「トレーニング開始時間を超過しています」というアナウンスと-->
-                        		<!--　「トレーニング終了」ボタンを表示-->
-                        		if(now > booking_date_from){
-                        		    if(bookingstatus_id[$i] == "1" || bookingstatus_id[$i] == "5"){
-                    		                if(now < booking_date_to){
-                                    		    $(check_in_out_id).append(
-                                        			`
-                                        			<h5>トレーニング開始時間を超過しています。<h5>
-                                        			<form id="check_out" method="put" action="check_out">
-                                        		        <input type="hidden" name="gym_id" value=${gym_id[$i]}>
-                                        		        <input type="hidden" name="booking_id" value=${booking_id[$i]}>
-                                        			    <input type="submit" class="button gray" style="width:100%; height:35px;" value="トレーニング終了">
-                                    			    </form>
-                                        			`
-                                    		    )
-                                		    }else {
-                                		        $(check_in_out_id).append(
-                                        			`
-                                        			<h5>トレーニング時間は終了しました。<h5>
-                                        			`
-                                        			)
-                                		    }
-                            		    }
-                            		}else{
-                            		    $(check_in_out_id).append(
-                                			`
-                                			<form id="check_in" method="put" action="check_in">
-                                		        <input type="hidden" name="gym_id" value=${gym_id[$i]}>
-                                		        <input type="hidden" name="booking_id" value=${booking_id[$i]}>
-                                			    <input type="submit" class="button gray" style="width:100%; height:35px;" value="トレーニング開始">
-                            			    </form>
-                                			`
-                            		    )
-                        		    }
-                    		    eq_num ++;
-                    		}
-                		}
-                		
-                		
-                		<!--// 「トレーニング開始」ボタンを押したら、「トレーニングを開始しますか？（この処理は取り消すことができません）」というconfirmを出した上で、-->
-                		<!--    check_in.blade.phpに遷移し、bookingsテーブルのbookingstatus_idを「20」に変更する（ルーティングが必要）-->
-                		$("#check_in").submit(function(){
-                		    if(window.confirm('トレーニングを開始しますか？（この処理は取り消すことができません）')){ // 確認ダイアログを表示
-                        		return true; // 「OK」時は送信を実行
-                        	}else{ // 「キャンセル」時の処理
-                        		return false; // 送信を中止
-            		        }
-                		});
-                		
-                		
-                		<!--// 「トレーニング終了」ボタンを押したら、「トレーニングを終了しますか？（この処理は取り消すことができません）」というconfirmを出した上で、-->
-                		<!--    checked_out.blade.phpに遷移し、bookingsテーブルのbookingstatus_idを「25」に変更する（ルーティングが必要）-->
-                		$("#check_out").submit(function(){
-                		    if(window.confirm('トレーニングを終了しますか？（この処理は取り消すことができません）')){ // 確認ダイアログを表示
-                        		return true; // 「OK」時は送信を実行
-                        	}else{ // 「キャンセル」時の処理
-                        		return false; // 送信を中止
-            		        }
-                		});
+                         
                 
                     }
                     
