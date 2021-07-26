5bb7135c2222368d2b4024e85ebebf31e66b590f
EC2 Default User <ec2-user@ip-172-31-3-253.ec2.internal>
2021-07-26 13:46:56 +0000

    menu_mobnavのmaximum-scale=1.0を設定

--- resources/views/layouts/menu_mobnav.blade.php
+++ resources/views/layouts/menu_mobnav.blade.php
@@ -6,7 +6,7 @@
 <title>Fandelion</title>
 <meta charset="utf-8">
 <!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">-->
-<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
 
 
 <link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css">
