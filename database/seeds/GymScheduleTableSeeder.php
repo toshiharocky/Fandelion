2af03a37c8b3a88d474d93d4a2bbbafefd6bcf55
EC2 Default User <ec2-user@ip-172-31-3-253.ec2.internal>
2022-01-02 17:31:57 +0000

    バグ修正

--- database/seeds/GymScheduleTableSeeder.php
+++ database/seeds/GymScheduleTableSeeder.php
@@ -37,10 +37,10 @@ class GymScheduleTableSeeder extends Seeder
             $day_start_time = "2022-01-06 10:00";
             $from_time = $day_start_time;
             $to_time = date("Y-m-d H:i", strtotime('+15 minute',strtotime($from_time)));
-            $day_end_time = "2021-01-06 23:00";
+            $day_end_time = "2022-01-06 23:00";
             
             
-            while (strtotime($day_start_time) < strtotime("2021-01-09 10:00")){
+            while (strtotime($day_start_time) < strtotime("2022-01-09 10:00")){
                 // 今日に日付から30日間、from_time00:00からto_time00:15まで、15分ずつのpriceとstatusを作成
                 while (strtotime($from_time) < strtotime($day_end_time)){
                     $price_array = array(50,100,150,200);
